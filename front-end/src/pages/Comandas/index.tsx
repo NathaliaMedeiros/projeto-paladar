a5cf4fd94d54d8004e7f0a9a23ec6581c242a7da
Jéssika <jessikafujimura@gmail.com>
2021-10-13 00:13:16 -0300

    Conversao de moeda real

--- front-end/src/pages/Comandas/index.tsx
+++ front-end/src/pages/Comandas/index.tsx
@@ -3,13 +3,18 @@ import { useEffect, useState } from "react";
 
 const Comandas = () => {
     const [numComanda, setNumComanda] = useState<number>();
-    const [listItens, setListItens] = useState<String[]>([]);
-    const [item, setItem] = useState<string>("");
+    const [listItens, setListItens] = useState<number[]>([]);
+    const [item, setItem] = useState<number>(0);
+    const [total, setTotal] = useState<number>(0);
 
-    function adicionarItem(item: String) {
+
+    function adicionarItem(item: number) {
+        const moeda = item.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        console.log(moeda)
         listItens.push(item)
         setListItens(listItens);
-        setItem("");
+        setItem(0);
+        setTotal(listItens.reduce((total, currentElement) => total+currentElement))
     }
 
     function gerarNumComanda() {
@@ -33,25 +38,24 @@ const Comandas = () => {
                     <div className="mb-4">
                         <label htmlFor="item-value" className="form-label">Adicionar item</label>
                         <div className="d-flex justify-content-between" >
-                            <input type="text" className="form-control" id="item-value" placeholder="R$" value={item} onChange={(e) => setItem(e.target.value.toString())} />
+                            <input type="text" className="form-control" id="item-value" placeholder="R$" value={item} onChange={(e) => setItem(Number(e.target.value))} />
                             <button type="button" className="btn btn-primary" onClick={() => adicionarItem(item)}>Adicionar</button>
                         </div>
                     </div>
                     <div className="mb-4">
                         <table className="table">
                             <tbody>
-                                {listItens.map((i: String) =>
-                                    <tr>
-                                        <td>{i}</td>
+                                {listItens.map((i:number,pos) =>
+                                    <tr key={pos + "item"}>
+                                        <td>{i.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
                                     </tr>
-
                                 )}
                             </tbody>
                         </table>
                     </div>
                     <div className="mb-4">
                         <h4 id="item-value">
-                            TOTAL R$
+                            TOTAL {total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
                         </h4>
                     </div>
                     <div className="d-flex justify-content-between">
